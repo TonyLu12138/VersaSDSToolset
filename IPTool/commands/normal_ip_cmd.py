96ff395ede5c791ca8373d9da3d6d84ecc6e8463
TonyLu12138 <138006045+TonyLu12138@users.noreply.github.com>
2023-10-26 15:36:42 +0800

    注释掉了normal_ip创建、删除和修改方法中ssh相关的参数

--- IPTool/commands/normal_ip_cmd.py
+++ IPTool/commands/normal_ip_cmd.py
@@ -11,16 +11,16 @@ class NormalIPCommands(object):
         subp_ip = self.parser_normal_ip.add_subparsers()
 
         parser_create = subp_ip.add_parser('create', aliases=['c'], help='Create IP')
-        parser_create.add_argument('-n',
-                                   '--node',
-                                   dest='node',
-                                   action='store',
-                                   help='Node (IP) for SSH connect')
-        parser_create.add_argument('-p',
-                                   '--password',
-                                   dest='password',
-                                   action='store',
-                                   help='Password for SSH connect')
+        # parser_create.add_argument('-n',
+        #                            '--node',
+        #                            dest='node',
+        #                            action='store',
+        #                            help='Node (IP) for SSH connect')
+        # parser_create.add_argument('-p',
+        #                            '--password',
+        #                            dest='password',
+        #                            action='store',
+        #                            help='Password for SSH connect')
         parser_create.add_argument('-d',
                                    '--device',
                                    dest='device',
@@ -36,16 +36,16 @@ class NormalIPCommands(object):
                                    help='Bonding ip')
 
         parser_delete = subp_ip.add_parser('delete', aliases=['d', 'del'], help='Delete IP')
-        parser_delete.add_argument('-n',
-                                   '--node',
-                                   dest='node',
-                                   action='store',
-                                   help='Node (IP) for SSH connect')
-        parser_delete.add_argument('-p',
-                                   '--password',
-                                   dest='password',
-                                   action='store',
-                                   help='Password for SSH connect')
+        # parser_delete.add_argument('-n',
+        #                            '--node',
+        #                            dest='node',
+        #                            action='store',
+        #                            help='Node (IP) for SSH connect')
+        # parser_delete.add_argument('-p',
+        #                            '--password',
+        #                            dest='password',
+        #                            action='store',
+        #                            help='Password for SSH connect')
         parser_delete.add_argument('-d',
                                    '--device',
                                    dest='device',
@@ -55,16 +55,16 @@ class NormalIPCommands(object):
                                    help='Device name')
 
         parser_modify = subp_ip.add_parser('modify', aliases=['m', 'mod'], help='Modify IP')
-        parser_modify.add_argument('-n',
-                                   '--node',
-                                   dest='node',
-                                   action='store',
-                                   help='Node (IP) for SSH connect')
-        parser_modify.add_argument('-p',
-                                   '--password',
-                                   dest='password',
-                                   action='store',
-                                   help='Password for SSH connect')
+        # parser_modify.add_argument('-n',
+        #                            '--node',
+        #                            dest='node',
+        #                            action='store',
+        #                            help='Node (IP) for SSH connect')
+        # parser_modify.add_argument('-p',
+        #                            '--password',
+        #                            dest='password',
+        #                            action='store',
+        #                            help='Password for SSH connect')
         parser_modify.add_argument('-ip',
                                    '--ip',
                                    dest='ip',
